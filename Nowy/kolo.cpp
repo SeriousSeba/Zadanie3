7697d8f905f2ef463bac5377a15f74d10cbad687
SeriousSeba <prykstefan@gmail.com>
2015-12-14 21:19:18 +0100

    1

---
+++ Nowy/kolo.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{double r;
+scanf("%lf", &r);
+printf("Pole kola o promieniu %.3lf wynosi %.3lf a jego obwod %.3lf",r, M_PI*r*r, 2*r*M_PI);
+}
---
+++ Nowy/prostopadloscian.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void)
+{int a,b,h;
+scanf("%d%d%d",&a,&b,&h);
+printf("Objetosc prostopadloscianu o bokach %d i %d i wysokosci %d wynosi %d a jego pole powierzchni %d ", a,b,h,a*b*h,2*a*b+2*a*h+2*b*h);
+}
